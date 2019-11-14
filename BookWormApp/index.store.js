b3eaaceb98400d47b51574f22a21784694d1b8c9
Akanksha Gupta <akanksha.gupta@drivezy.com>
2019-11-14 11:07:46 +0530

    All files

--- BookWormApp/index.store.js
+++ BookWormApp/index.store.js
@@ -1,19 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 
 import AppReducer from './Reducer/index.reducer';
-//import RootSaga from './Sagas/index.saga';
 
-//const sagaMiddleware = createSagaMiddleware();
-
-//moddleware array build...
-// const middlewares = (__DEV__) ? [sagaMiddleware, logger] : [sagaMiddleware];
-// const middlewares = (global.__DEV__) ? [sagaMiddleware, logger] : [sagaMiddleware];
-
-//const middlewares = [sagaMiddleware];
-
-// let store = createStore(AppReducer);
 const store = createStore(AppReducer);
-
-//sagaMiddleware.run(RootSaga);
 
 export default store;
